cad7db300bc8fa16219659f35f8430ce7d2f0f51
kennja05 <jacobkenny05@gmail.com>
2020-01-26 16:59:44 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-// Write your solution here!
+// Write your solution here
+let drivers = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendDriver(name) {
+    drivers.push(name);
+}
+
+function destructivelyPrependDriver(name) {
+    drivers.unshift(name);
+}
+
+function destructivelyRemoveLastDriver() {
+    drivers.pop();
+}
+
+function destructivelyRemoveFirstDriver() {
+    drivers.shift();
+}
+
+function appendDriver(name) {
+    let my_array = drivers.slice(0);
+    my_array.push(name);
+    return my_array;
+}
+
+function prependDriver(name) {
+    let my_array = drivers.slice(0);
+    my_array.unshift(name);
+    return my_array;
+}
+
+function removeFirstDriver() {
+    let my_array = drivers.slice(0);
+    my_array.shift();
+    return my_array;
+}
+
+function removeLastDriver() {
+    let my_array = drivers.slice(0);
+    my_array.pop();
+    return my_array;
+}
